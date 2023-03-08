3b973a8953313270796e91553dff3d303869d092
ColdWape <92642047+ColdWape@users.noreply.github.com>
2023-03-08 21:52:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request
+import csv
 import os
+
+def write_datas_into_file(datas):
+    File = open('datas/sentiments.csv', 'a', encoding = 'UTF-8', newline = '')
+    Writer = csv.writer(File, delimiter = '\t')
+    Writer.writerow(datas)
+    File.close()
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/' ,methods=['POST','GET'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == "POST":
+        text = request.form['sentiment_text']
+        sentiment = request.form['sentiment']
+        try:
+            write_datas_into_file([text,sentiment])
+            return render_template("index.html",added_sentiment=True)
+        except:
+            return "Ошибка"
+    else:
+        return render_template("index.html",added_sentiment=False)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ == "__main__":
+    app.run(debug=False,port=os.getenv("PORT", default=5000))
+
+
+
